2a5e14aca6010f80ed003fe0dc1c2629f30a9eed
allenx555 <allenx555@163.com>
2019-04-13 15:49:18 +0800

    feat: add footer

---
+++ src/components/footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer>
+        <a herf="#">Home      </a>
+        <a herf="#">Products      </a>
+        <a herf="#">Privacy      </a>
+        <a herf="#">Terms      </a>
+        <a herf="#">Site Maps      </a>
+        <a herf="#">Links      </a>
+      </footer>
+    );
+  }
+}
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -6,40 +6,49 @@ const SubMenu = Menu.SubMenu;
 class Header extends React.Component {
   render() {
     return (
-      <Menu
-        onClick={this.handleClick}
-        selectedKeys={[this.state.current]}
-        mode="horizontal"
-      >
-        <SubMenu
-          title={
-            <span className="submenu-title-wrapper">
-              <Icon type="appstore" />
-              language
-            </span>
-          }
-        >
-          <Menu.Item key="setting:1">Chinese</Menu.Item>
-        </SubMenu>
-        <Menu.Item key="settings" disabled>
-          <Icon type="setting" />
-          settings
-        </Menu.Item>
-        <Menu.Item key="support" disabled>
-          <Icon type="question-circle" />
-          support
-        </Menu.Item>
-        <Menu.Item key="us">
-          <a
-            href="https://jotang.party"
-            target="_blank"
-            rel="noopener noreferrer"
+      <div className="header">
+        <div className="header1">
+          <img src="#" />
+          <a herf="#">Home |</a>
+          <a herf="#"> Analaysis</a>
+        </div>
+        <div className="header2">
+          <Menu
+            onClick={this.handleClick}
+            selectedKeys={[this.state.current]}
+            mode="horizontal"
           >
-            <Icon type="team" />
-            us
-          </a>
-        </Menu.Item>
-      </Menu>
+            <SubMenu
+              title={
+                <span className="submenu-title-wrapper">
+                  <Icon type="appstore" />
+                  language
+                </span>
+              }
+            >
+              <Menu.Item key="setting:1">Chinese</Menu.Item>
+            </SubMenu>
+            <Menu.Item key="settings" disabled>
+              <Icon type="setting" />
+              settings
+            </Menu.Item>
+            <Menu.Item key="support" disabled>
+              <Icon type="question-circle" />
+              support
+            </Menu.Item>
+            <Menu.Item key="us">
+              <a
+                href="https://jotang.party"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Icon type="team" />
+                us
+              </a>
+            </Menu.Item>
+          </Menu>
+        </div>
+      </div>
     );
   }
 }
