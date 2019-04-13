2743544c4f3f41e3c35798dcaa5ec0fabd8a444b
allenx555 <allenx555@163.com>
2019-04-13 15:06:44 +0800

    feat: add header

---
+++ src/components/header.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { Menu, Icon } from "antd";
+
+const SubMenu = Menu.SubMenu;
+
+class Header extends React.Component {
+  render() {
+    return (
+      <Menu
+        onClick={this.handleClick}
+        selectedKeys={[this.state.current]}
+        mode="horizontal"
+      >
+        <SubMenu
+          title={
+            <span className="submenu-title-wrapper">
+              <Icon type="appstore" />
+              language
+            </span>
+          }
+        >
+          <Menu.Item key="setting:1">Chinese</Menu.Item>
+        </SubMenu>
+        <Menu.Item key="settings" disabled>
+          <Icon type="setting" />
+          settings
+        </Menu.Item>
+        <Menu.Item key="support" disabled>
+          <Icon type="question-circle" />
+          support
+        </Menu.Item>
+        <Menu.Item key="us">
+          <a
+            href="https://jotang.party"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Icon type="team" />
+            us
+          </a>
+        </Menu.Item>
+      </Menu>
+    );
+  }
+}
