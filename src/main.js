1961eab1d48dc6842b6210fd542520444ce0f7c8
allenx555 <allenx555@163.com>
2019-04-14 15:58:10 +0800

    feat: use parcel

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { Router } from "react-router";
+
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById("container")
+);
