b2d3ee90b304236141cb67a474124a3ed6370703
allenx555 <allenx555@163.com>
2019-04-17 16:19:47 +0800

    feat: add axios

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import history from "./history";
 import { Router } from "react-router";
 
 ReactDOM.render(
-  <Router>
+  <Router history={history}>
     <App />
   </Router>,
   document.getElementById("container")
