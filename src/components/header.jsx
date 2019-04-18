588282bebbae59355e484820e968e79ef6460915
allenx555 <allenx555@163.com>
2019-04-18 23:12:38 +0800

    feat: add babel to fix bugs

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,7 +3,17 @@ import { Menu, Icon } from "antd";
 
 const SubMenu = Menu.SubMenu;
 
-class Header extends React.Component {
+export default class Header extends React.Component {
+  state = {
+    current: 'mail',
+  }
+
+  handleClick = (e) => {
+    console.log('click ', e);
+    this.setState({
+      current: e.key,
+    });
+  }
   render() {
     return (
       <div className="header">
--- src/views/NotFound.jsx
+++ src/views/NotFound.jsx
@@ -1,21 +1,30 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Header from "../components/header";
+import Footer from "../components/footer";
 
 const PageNotFound = "https://i.loli.net/2018/11/14/5bec09142d118.gif";
 
-const NotFound = () => (
-  <div>
-    <img
-      src={PageNotFound}
-      style={{
-        display: "block",
-        margin: "auto",
-        position: "relative"
-      }}
-    />
-    <center>
-      <Link to="/home">Return to Home Page</Link>
-    </center>
-  </div>
-);
-export default NotFound;
+export default class NotFound extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <div>
+          <img
+            src={PageNotFound}
+            style={{
+              display: "block",
+              margin: "auto",
+              position: "relative"
+            }}
+          />
+          <center>
+            <Link to="/home">Return to Home Page</Link>
+          </center>
+        </div>
+        <Footer />
+      </div>
+    )
+  }
+}
