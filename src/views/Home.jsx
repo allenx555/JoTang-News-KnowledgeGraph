b19871dabf96316194a13743694b7bbacf18da4f
allenx555 <allenx555@163.com>
2019-04-24 22:08:28 +0800

    feat: change inputbox to antd.input

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,17 +1,25 @@
 import React from "react";
 import Header from "../components/header";
 import Footer from "../components/footer";
-import searchinput from "../components/searchinput";
+import { Input } from "antd";
+import "../assets/home.scss"
 
+const Search = Input.Search;
 const logo = "http://pic.555dreams.xin/images/2019/04/20/1.png";
 export default class Home extends React.Component {
   render() {
     return (
       <div class="container">
         <Header />
-        <img src={logo} alt="logo"/>
+        <img src={logo} alt="logo" id="logo1"/>
         <div class="content">
-          <div id="newsinfo" />
+          <div id="newsinfo">
+            <Search
+              placeholder="input search text"
+              onSearch={value => console.log(value)}
+              style={{ width: 200 }}
+            />
+          </div>
           <div id="newsdetail" />
           <div id="morenews" />
         </div>
--- src/views/SearchResult.jsx
+++ src/views/SearchResult.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import SearchInput from "../components/searchinput";
 import Header from "../components/header";
 import Footer from "../components/footer";
 import "../assets/searchresult.scss";
