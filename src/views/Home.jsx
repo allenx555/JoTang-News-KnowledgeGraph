93f13fdcbca29bebdf8841aec8232f7453e71da9
allenx555 <allenx555@163.com>
2019-05-26 17:09:47 +0800

    feat: add test data

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -3,35 +3,17 @@ import Header from "../components/header";
 import Footer from "../components/footer";
 import { Input, Carousel, Card, Row, Col } from "antd";
 import "../assets/home.scss";
+import axios from "axios";
+import testjson from "../static/test.json";
 
 const Search = Input.Search;
 const logo = "http://pic.555dreams.xin/images/2019/04/20/1.png";
 
-const tabListNoTitle = [
-  {
-    key: "article",
-    tab: "article"
-  },
-  {
-    key: "app",
-    tab: "app"
-  },
-  {
-    key: "project",
-    tab: "project"
-  }
-];
-
-const contentListNoTitle = {
-  article: <p>article content</p>,
-  app: <p>app content</p>,
-  project: <p>project content</p>
-};
-
 export default class Home extends React.Component {
   state = {
     key: "tab1",
-    noTitleKey: "app"
+    noTitleKey: "app",
+    content: testjson
   };
 
   onTabChange = (key, type) => {
@@ -39,6 +21,14 @@ export default class Home extends React.Component {
     this.setState({ [type]: key });
   };
 
+  /*componentDidMount() {
+    const _this = this;
+    axios.get("../static/test.json").then(function(response) {
+      _this.setState({ content: response});
+      console.log(JSON.stringify(response.data));
+    });
+  }
+  */
   render() {
     return (
       <div className="container">
@@ -48,62 +38,70 @@ export default class Home extends React.Component {
           <div id="newsinfo">
             <div id="pics">
               <Carousel autoplay>
-                <div>
-                  <h3>1</h3>
-                </div>
-                <div>
-                  <h3>2</h3>
-                </div>
-                <div>
-                  <h3>3</h3>
-                </div>
-                <div>
-                  <h3>4</h3>
-                </div>
+                {this.state.content.pic.map(item => {
+                  return (
+                    <div>
+                      <img
+                        src={item.picurl}
+                        alt={item.title}
+                        className="largenews"
+                      />
+                    </div>
+                  );
+                })}
               </Carousel>
             </div>
             <div id="search">
               <Search
                 placeholder="input search text"
                 onSearch={value => console.log(value)}
-                style={{ width: 300 }}
               />
-              <Card
-                title="Most Viewed"
-                extra={<a href="#">More</a>}
-                style={{ width: 300 }}
-                id="card2"
-              >
-                <p>Card content</p>
-                <p>Card content</p>
-                <p>Card content</p>
+              <Card title="Most Viewed" extra={<a href="#">More</a>} id="card2">
+                {this.state.content.MostViewed.map(item => {
+                  return (
+                    <div className="ellipsis">
+                      <a
+                        href={item.url}
+                        style={{ color: "rgba(0, 0, 0, 0.8)", "font-size": 14 }}
+                      >
+                        <u>{item.title}</u>
+                      </a>
+                    </div>
+                  );
+                })}
               </Card>
             </div>
             <div id="main">
-              <Card title="Headline" style={{ width: 460 }} id="card1">
-                <p>Card content</p>
-                <p>Card content</p>
-                <p>Card content</p>
+              <Card
+                title={this.state.content.Headline.title}
+                style={{ width: 460 }}
+                id="card1"
+              >
+                <p id="maincontent">{this.state.content.Headline.content}</p>
               </Card>
             </div>
           </div>
           <div id="morenews">
             <Row gutter={16} style={{ width: 1200 }}>
-              <Col span={3}><div id="head-title">Other News</div></Col>
-              <Col span={5} title="Card title">
-                <Card style={{ height: 150 }}>Card content</Card>
+              <Col span={3}>
+                <div id="head-title">Other News</div>
               </Col>
-              <Col span={4}>
-                <Card style={{ height: 150 }}>Card content</Card>
-              </Col>
-              <Col span={4}>
-                <Card style={{ height: 150 }}>Card content</Card>
-              </Col>
-              <Col span={4}>
-                <Card style={{ height: 150 }}>Card content</Card>
-              </Col>
-              <Col span={4}>
-                <Card style={{ height: 150 }}>Card content</Card>
+              {this.state.content.OtherNews.map(item => {
+                return (
+                  <Col span={3}>
+                    <div className="ellipsis">
+                      <img
+                        src={item.picurl}
+                        alt={item.title}
+                        className="smallnews"
+                      />
+                      <p>{item.title}</p>
+                    </div>
+                  </Col>
+                );
+              })}
+              <Col span={2}>
+                <video src="test" controls="controls" />
               </Col>
             </Row>
           </div>
